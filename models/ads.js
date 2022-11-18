9b48a3440d90d2d35347f4b83aff9d605ea5456a
Rajat <remixrajat1@gmail.com>
2022-11-18 23:53:37 +0530

    removed comments

--- models/ads.js
+++ models/ads.js
@@ -1,11 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-// mongoose.connect("mongodb://127.0.0.1:27017/adds-add-api", {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
-
 const adSchema = new Schema({
   _id: {
     type: Number,
@@ -33,18 +28,3 @@ const adSchema = new Schema({
 });
 
 module.exports = mongoose.model("Ad", adSchema);
-
-// const cpny = new Company({
-//   _id: 1,
-//   name: "Levi's",
-//   url: "http://levis.com/",
-// });
-
-// cpny
-//   .save()
-//   .then(() => {
-//     console.log(cpny);
-//   })
-//   .catch((error) => {
-//     console.log("Error", error);
-//   });
--- models/companies.js
+++ models/companies.js
@@ -1,11 +1,6 @@
 const mongoose = require("mongoose");
 const { Schema } = require("mongoose");
 
-// mongoose.connect("mongodb://127.0.0.1:27017/adds-add-api", {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
-
 const companySchema = new Schema({
   _id: {
     type: Number,
@@ -19,18 +14,3 @@ const companySchema = new Schema({
 });
 
 module.exports = mongoose.model("Company", companySchema);
-
-// const cpny = new Company({
-//   _id: 1,
-//   name: "Levi's",
-//   url: "http://levis.com/",
-// });
-
-// cpny
-//   .save()
-//   .then(() => {
-//     console.log(cpny);
-//   })
-//   .catch((error) => {
-//     console.log("Error", error);
-//   });
